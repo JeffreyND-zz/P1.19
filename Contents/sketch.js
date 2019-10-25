0b230e968dd1953b3acdd98dded66a2365607282
JeffreyND <jeffreyndimele@yahoo.com>
2019-10-24 20:06:21 -0500

    file changes

--- Contents/sketch.js
+++ Contents/sketch.js
@@ -1,7 +1,33 @@
+let capture;
+let img;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+  capture = createCapture(VIDEO);
+  capture.hide();
+}
+
+function preload(){
+  img = loadImage('assets/Left_Health.png');
 }
 
 function draw() {
-  background(220);
+  image(capture, 0, 0, 480, 360);
+  fill(color(255, 204, 0));
+  rect(10, 10, 10, 340);
+  rect(460, 10, 10, 340);
+  fill(color(255, 255, 255)); 
+  image(img, 60, 60, 60, 50);
+  circle(60, 50, 60, 60);
+  circle(60, 175, 60, 60);
+  circle(60, 300, 60, 60);
+  
+  
+    //clock setion start
+  sec = second();
+  minut = minute();
+  hou = hour();
+  text(sec+':'+minut+':'+hou, 3,10);
+  textSize(15);
+  //clocksection end
 }
